e4aed95638470ac3f795a53761b28cf1018a13f5
unknown <nadya.ershov4@yandex.ru>
2021-07-30 22:52:51 +0300

    webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import './index.css';
 import { FormValidator } from "../components/FormValidator.js";
 import { Card } from "../components/Card.js";
 import { initialCards } from "../utils/initial-cards.js";
